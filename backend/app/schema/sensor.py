800a23604b3f6cae18372857fd428080bf8a9364
장우주 <jangone12@naver.com>
2025-05-21 16:50:15 +0900

    14:45 우주

--- backend/app/schema/sensor.py
+++ backend/app/schema/sensor.py
@@ -6,10 +6,12 @@ from datetime import datetime
 # ✅ 센서 데이터 수신용 (POST)
 class SensorDataRequest(BaseModel):
     se_idx: int = Field(..., description="센서 장비 식별번호")
-    temp: float = Field(..., description="감지된 온도")
-    humidity: float = Field(..., description="감지된 습도")
-    pm10: float = Field(..., description="감지된 미세먼지 PM10")
-    pm25: float = Field(..., description="감지된 미세먼지 PM2.5")
+    # temp: Optional[float] = Field(..., description="감지된 온도")
+    temp: Optional[float] = Field(None)
+    # humidity: Optional[float] = Field(..., description="감지된 습도")
+    humidity: Optional[float] = Field(None)
+    pm10: Optional[float] = Field(..., description="감지된 미세먼지 PM10")
+    pm25: Optional[float] = Field(..., description="감지된 미세먼지 PM2.5")
     outlier: bool = Field(..., description="이상치 여부")
 
 # ✅ 센서 응답용
@@ -20,12 +22,12 @@ class SensorDataResponse(BaseModel):
 # ✅ DB 저장 및 조회용
 class SensorData(BaseModel):
     sd_idx: int = Field(..., description="센서 데이터 식별번호 (PK)")
-    se_idx: int = Field(..., description="센서 장비 식별번호 (FK)")
-    created_at: datetime = Field(..., description="데이터 수집 일시")
-    temp: float = Field(..., description="감지된 온도")
-    humidity: float = Field(..., description="감지된 습도")
-    pm10: float = Field(..., description="감지된 PM10")
-    pm25: float = Field(..., description="감지된 PM2.5")
+    se_idx: Optional[int] = Field(..., description="센서 장비 식별번호 (FK)")
+    created_at: Optional[datetime] = Field(..., description="데이터 수집 일시")
+    temp: Optional[float] = Field(..., description="감지된 온도")
+    humidity: Optional[float] = Field(..., description="감지된 습도")
+    pm10: Optional[float] = Field(..., description="감지된 PM10")
+    pm25: Optional[float] = Field(..., description="감지된 PM2.5")
     outlier: bool = Field(..., description="이상치 여부")
 
     class Config:
